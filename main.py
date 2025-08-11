1dd46b3b139a7e5d9f444269077786f271290f97
Lucas Viccari <97442300+Vicca1511@users.noreply.github.com>
2025-08-11 19:47:19 -0300

    update branch

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World from no where"}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Branch de construção paralela! "}    
